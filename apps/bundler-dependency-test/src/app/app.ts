6ec15754aba9a35839323179e41239ca48ece534
Matthias Biebl <matthias.biebl@datev.de>
2025-07-08 16:32:10 +0200

    my external neu generieren

--- apps/bundler-dependency-test/src/app/app.ts
+++ apps/bundler-dependency-test/src/app/app.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
-import { MyInternal } from '@bundler-dependency-test/my-internal';
-import { MyOtherLib } from '@bundler-dependency-test/my-other-lib';
+import { MyExternal } from '@bundler-dependency-test/my-external';
 
 @Component({
-  imports: [NxWelcome, RouterModule, MyInternal, MyOtherLib],
+  imports: [NxWelcome, RouterModule, MyExternal],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected title = 'bundler-dependency-test';
-}
+export class App {}
